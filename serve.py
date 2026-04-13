86aeec3fce8dfe41b1daba75157207f98b60c075
3csbeezneez <3csbeezneez@gmail.com>
2026-04-13 07:53:37 +0000

    first experiment: hand-connect and finger-frame panel

---
+++ serve.py
@@ -0,0 +1,9 @@
+import http.server, ssl, os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+server = http.server.HTTPServer(('0.0.0.0', 8443), http.server.SimpleHTTPRequestHandler)
+ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ctx.load_cert_chain('cert.pem', 'key.pem')
+server.socket = ctx.wrap_socket(server.socket, server_side=True)
+print("Serving at https://localhost:8443/hand-panel.html")
+server.serve_forever()
